ef227ce2f02243433736796338ddc835bcd82a5a
Lucas Vergara <lucasbeskow@gmail.com>
2026-04-13 19:14:50 -0300

    JS + API

--- scripts.js
+++ scripts.js
@@ -113,4 +113,29 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
 
 // buscarDados();
 
+/*
+// Notification
+// Verifica se o navegador suporta a API de Notificação
+if ("Notification" in window) {
+  // Solicita permissão ao usuário para enviar notificações
+  Notification.requestPermission().then(permission => {
+    if (permission === "granted") {
+      // Se a permissão for concedida, cria uma nova notificação
+      const notificacao = new Notification("Título da Notificação", {
+        body: "Esta é a mensagem da notificação.",
+        icon: "https://placehold.co/64x64/0056b3/ffffff?text=JS"
+      });
+      
+      // Adiciona um evento de clique à notificação
+      notificacao.onclick = () => {
+        console.log("A notificação foi clicada!");
+      };
+    } else {
+      console.log("Permissão para notificações negada.");
+    }
+  });
+} else {
+  console.log("Este navegador não suporta a API de Notificação.");
+}
+  */
 
